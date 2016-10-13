dd6c292af926722c8acb96866fc0c74f3c29560c
TonyM <antonio@alchimia.mx>
2016-10-13 14:14:03 -0500

    proyect init

---
+++ index.ios.js
@@ -0,0 +1,60 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  ScrollView,
+  View,
+  Image,
+  Map
+} from 'react-native';
+
+export default class proyect extends Component {
+  render() {
+    // return (
+    //   <View style = {styles.container}>
+    //     <Text style = {styles.welcome}>
+    //       Welcome to Cinemex Auditory App
+    //     </Text>
+    //   </View>
+    // );
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return (
+      <View style = {styles.container}>
+        <Image source ={pic} style={{width: 193, height: 110, marginTop: 1, marginBottom: 35,}}/>
+        <Text style = {styles.welcome}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 12,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('proyect', () => proyect);
