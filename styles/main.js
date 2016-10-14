d15e26d4780ab540e040957ada29c5acc20c14e7
TonyM <antonio@alchimia.mx>
2016-10-14 12:19:39 -0500

    backup

---
+++ styles/main.js
@@ -0,0 +1,28 @@
+import {
+  StyleSheet
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+      fontSize: 12,
+      textAlign: 'center',
+      margin: 10,
+    },
+    instructions: {
+      textAlign: 'center',
+      color: '#333333',
+      marginBottom: 5,
+    },
+    img: {
+      width: 150,
+      height: 150,
+    },
+  });
+
+  module.exports = styles
